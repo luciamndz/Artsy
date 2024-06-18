591025d823f7450b82d61ca710b332ca7aaf0d88
jose tomas ruiz <j_tomas_93@hotmail.com>
2024-06-18 12:29:39 -0400

    trabajando en navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: :user_name)
-    devise_parameter_sanitizer.permit(:account_update, keys: :user_name)
-  end
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: :user_name)
+  #   devise_parameter_sanitizer.permit(:account_update, keys: :user_name)
+  # end
 end
