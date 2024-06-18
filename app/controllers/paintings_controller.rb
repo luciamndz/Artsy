c50ead64aa75f06b2c820f5237289594f464f162
Lucia Mendoza <almm.arquitectura@gmail.com>
2024-06-18 09:12:10 -0600

    Added has_one_attached in model painting

--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -1,2 +1,44 @@
 class PaintingsController < ApplicationController
+
+  before_action :find_painting, only: [:show, :edit, :update, :destroy]
+  def index #homepage
+    @paintings = Painting.all
+  end
+
+  def show
+  end
+  def new
+    @painting = Painting.new
+  end
+  def create
+    @painting = Painting.new(params_painting)
+    if @painting.save!
+      redirect_to painting_path(@painting)
+    else
+      render new:, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+  def update
+    @painting.update!(params_painting)
+    redirect_to painting_path
+  end
+
+  def destroy
+    @painting.destroy!
+    redirect_to paintings_path
+  end
+
+  private
+
+  def params_painting
+    params.require(:painting).permit(:title, :price, :measures, :user_id)
+  end
+
+  def find_painting
+    @painting = @painting = Painting.find(params[:id])
+  end
+
 end
